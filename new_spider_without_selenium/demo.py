f8a3c29b0bc505553b4131f17a07e582e1a99548
longxiaofei <523461639@qq.com>
2019-05-31 19:12:45 +0800

    1.修改了返回数据的形式 2.可以传入多个keywords 3.请求失败可以重新请求 4.重写了一部分代码

--- new_spider_without_selenium/demo.py
+++ new_spider_without_selenium/demo.py
@@ -1,22 +1,7 @@
 from get_index import BaiduIndex
 
 if __name__ == "__main__":
-    """
-    最多一次请求5个关键词
-    """
-    # 查看城市和省份的对应代码
-    print(BaiduIndex.city_code)
-    print(BaiduIndex.province_code)
-
-    baidu_index = BaiduIndex(['张艺兴', 'lol', '极限挑战', '吃鸡'], '2016-10-01', '2018-10-02')
-    for data in baidu_index('lol', 'all'):
-        print(data)
-
-    # 获取全部5个关键词的全部数据
-    print(baidu_index.result)
-    # 获取1个关键词的全部数据
-    print(baidu_index.result['极限挑战'])
-    # 获取1个关键词的移动端数据
-    print(baidu_index.result['极限挑战']['wise'])
-    # 获取1个关键词的pc端数据
-    print(baidu_index.result['极限挑战']['pc'])
+    keywords = ['爬虫', 'lol', '张艺兴', '人工智能', '华为', '武林外传']
+    baidu_index = BaiduIndex(keywords, '2018-01-01', '2019-05-02')
+    for index in baidu_index.get_index():
+        print(index)
--- new_spider_without_selenium/get_index.py
+++ new_spider_without_selenium/get_index.py
@@ -1,9 +1,15 @@
-from config import COOKIES, PROVINCE_CODE, CITY_CODE
 from urllib.parse import urlencode
-from collections import defaultdict
+import queue
+import math
 import datetime
-import requests
+import random
+import time
 import json
+
+import requests
+
+from config import COOKIES, PROVINCE_CODE, CITY_CODE
+
 
 headers = {
     'Host': 'index.baidu.com',
@@ -16,7 +22,7 @@ headers = {
 class BaiduIndex:
     """
         百度搜索指数
-        :keywords; list or string '<keyword>,<keyword>'
+        :keywords; list
         :start_date; string '2018-10-02'
         :end_date; string '2018-10-02'
         :area; int, search by cls.province_code/cls.city_code
@@ -24,41 +30,81 @@ class BaiduIndex:
 
     province_code = PROVINCE_CODE
     city_code = CITY_CODE
+    _all_kind = ['all', 'pc', 'wise']
+    _params_queue = queue.Queue()
 
-    def __init__(self, keywords, start_date, end_date, area=0):
-        """
-        """
-        self._keywords = keywords if isinstance(keywords, list) else keywords.split(',')
-        self._time_range_list = self.get_time_range_list(start_date, end_date)
-        self._all_kind = ['all', 'pc', 'wise']
+    def __init__(self, keywords: list, start_date: str, end_date: str, area=0):
+        self.keywords = keywords
         self._area = area
-        self.result = {keyword: defaultdict(list) for keyword in self._keywords}
-        self.get_result()
+        self._init_queue(start_date, end_date, keywords)
 
-    def get_result(self):
+    def get_index(self):
         """
+        获取百度指数
+        返回的数据格式为:
+        {
+            'keyword': '武林外传',
+            'type': 'wise',
+            'date': '2019-04-30',
+            'index': '202'
+        }
         """
-        for start_date, end_date in self._time_range_list:
-            encrypt_datas, uniqid = self.get_encrypt_datas(start_date, end_date)
-            key = self.get_key(uniqid)
-            for encrypt_data in encrypt_datas:
-                for kind in self._all_kind:
-                    encrypt_data[kind]['data'] = self.decrypt_func(key, encrypt_data[kind]['data'])
-                self.format_data(encrypt_data)
+        while 1:
+            try:
+                params_data = self._params_queue.get(timeout=1)
+                encrypt_datas, uniqid = self._get_encrypt_datas(
+                    start_date=params_data['start_date'],
+                    end_date=params_data['end_date'],
+                    keywords=params_data['keywords']
+                )
+                key = self._get_key(uniqid)
+                for encrypt_data in encrypt_datas:
+                    for kind in self._all_kind:
+                        encrypt_data[kind]['data'] = self._decrypt_func(
+                                key, encrypt_data[kind]['data'])
+                    for formated_data in self._format_data(encrypt_data):
+                        yield formated_data
+            except requests.Timeout:
+                self._params_queue.put(params_data)
+            except queue.Empty:
+                break
+            self._sleep_func()
 
-    def get_encrypt_datas(self, start_date, end_date):
+    def _init_queue(self, start_date, end_date, keywords):
+        """
+            初始化参数队列
+        """
+        keywords_list = self._split_keywords(keywords)
+        time_range_list = self._get_time_range_list(start_date, end_date)
+        for start_date, end_date in time_range_list:
+            for keywords in keywords_list:
+                params = {
+                    'keywords': keywords,
+                    'start_date': start_date,
+                    'end_date': end_date
+                }
+                self._params_queue.put(params)
+
+    def _split_keywords(self, keywords: list) -> [list]:
+        """
+        一个请求最多传入5个关键词, 所以需要对关键词进行切分
+        """
+        return [keywords[i*5: (i+1)*5] for i in range(math.ceil(len(keywords)/5))]
+
+    def _get_encrypt_datas(self, start_date, end_date, keywords):
         """
         :start_date; str, 2018-10-01
         :end_date; str, 2018-10-01
+        :keyword; list, ['1', '2', '3']
         """
         request_args = {
-            'word': ','.join(self._keywords),
-            'startDate': start_date,
-            'endDate': end_date,
+            'word': ','.join(keywords),
+            'startDate': start_date.strftime('%Y-%m-%d'),
+            'endDate': end_date.strftime('%Y-%m-%d'),
             'area': self._area,
         }
         url = 'http://index.baidu.com/api/SearchApi/index?' + urlencode(request_args)
-        html = self.http_get(url)
+        html = self._http_get(url)
         datas = json.loads(html)
         uniqid = datas['data']['uniqid']
         encrypt_datas = []
@@ -66,50 +112,51 @@ class BaiduIndex:
             encrypt_datas.append(single_data)
         return (encrypt_datas, uniqid)
 
-    def get_key(self, uniqid):
+    def _get_key(self, uniqid):
         """
         """
         url = 'http://index.baidu.com/Interface/api/ptbk?uniqid=%s' % uniqid
-        html = self.http_get(url)
+        html = self._http_get(url)
         datas = json.loads(html)
         key = datas['data']
         return key
 
-    def format_data(self, data):
+    def _format_data(self, data):
         """
+            格式化堆在一起的数据
         """
         keyword = str(data['word'])
-        time_len = len(data['all']['data'])
+        time_length = len(data['all']['data'])
         start_date = data['all']['startDate']
         cur_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
-        for i in range(time_len):
+        for i in range(time_length):
             for kind in self._all_kind:
                 index_datas = data[kind]['data']
                 index_data = index_datas[i] if len(index_datas) != 1 else index_datas[0]
                 formated_data = {
+                    'keyword': keyword,
+                    'type': kind,
                     'date': cur_date.strftime('%Y-%m-%d'),
                     'index': index_data if index_data else '0'
                 }
-                self.result[keyword][kind].append(formated_data)
-
+                yield formated_data
             cur_date += datetime.timedelta(days=1)
 
-    def __call__(self, keyword, kind='all'):
-        return self.result[keyword][kind]
-
-    @staticmethod
-    def http_get(url, cookies=COOKIES):
-        headers['Cookie'] = cookies
-        response = requests.get(url, headers=headers)
-        if response.status_code == 200:
-            return response.text
-        else:
-            return None
-
-    @staticmethod
-    def get_time_range_list(startdate, enddate):
+    def _http_get(self, url, cookies=COOKIES):
         """
-        max 6 months
+            发送get请求, 程序中所有的get都是调这个方法
+            如果想使用多cookies抓取, 和请求重试功能
+            在这自己添加
+        """
+        headers['Cookie'] = cookies
+        response = requests.get(url, headers=headers, timeout=5)
+        if response.status_code != 200:
+            raise requests.Timeout
+        return response.text
+
+    def _get_time_range_list(self, startdate, enddate):
+        """
+            切分时间段
         """
         date_range_list = []
         startdate = datetime.datetime.strptime(startdate, '%Y-%m-%d')
@@ -117,16 +164,15 @@ class BaiduIndex:
         while 1:
             tempdate = startdate + datetime.timedelta(days=300)
             if tempdate > enddate:
-                all_days = (enddate-startdate).days
                 date_range_list.append((startdate, enddate))
-                return date_range_list
+                break
             date_range_list.append((startdate, tempdate))
             startdate = tempdate + datetime.timedelta(days=1)
+        return date_range_list
 
-    @staticmethod
-    def decrypt_func(key, data):
+    def _decrypt_func(self, key, data):
         """
-        decrypt data
+            数据解密方法
         """
         a = key
         i = data
@@ -138,5 +184,9 @@ class BaiduIndex:
             s.append(n[i[r]])
         return ''.join(s).split(',')
 
-if __name__ == '__main__':
-    pass
+    def _sleep_func(self):
+        """
+            sleep方法, 单账号抓取过快, 一段时间内请求会失败
+        """
+        sleep_time = random.choice(range(50, 90)) * 0.1
+        time.sleep(sleep_time)
