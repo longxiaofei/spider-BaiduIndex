3f7b17238fe588a1339ad7e66bbb5dbb12e1ac4a
longxiaofei <libra.19951002@gmail.com>
2021-03-25 23:21:53 +0800

    MOD: 修改setup配置

--- setup.py
+++ setup.py
@@ -1,131 +1,41 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# Note: To use the 'upload' functionality of this file, you must:
-#   $ pipenv install twine --dev
-
-import io
+import setuptools
 import os
-import sys
-from shutil import rmtree
 
-from setuptools import find_packages, setup, Command
+VERSION = '0.1.2'
 
-# Package meta-data.
-NAME = 'qdata'
-DESCRIPTION = 'Python SDK for getting data quickly'
-URL = 'https://github.com/longxiaofei/spider-BaiduIndex'
-EMAIL = 'libra.19951002@gmail.com'
-AUTHOR = 'longxiaofei'
-REQUIRES_PYTHON = '>=3.7.0'
-VERSION = '0.1.1'
+CUR_DIR = os.path.abspath(os.path.dirname(__file__))
+README = os.path.join(CUR_DIR, "README.md")
+with open("README.md", "r") as fd:
+    long_description = fd.read()
 
-# What packages are required for this module to be executed?
-REQUIRED = [
-    'requests>=2.19.1'
-]
-
-# What packages are optional?
-EXTRAS = {
-    # 'fancy feature': ['django'],
-}
-
-# The rest you shouldn't have to touch too much :)
-# ------------------------------------------------
-# Except, perhaps the License and Trove Classifiers!
-# If you do change the License, remember to change the Trove Classifier for that!
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
-try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
-
-# Load the package's __version__.py module as a dictionary.
-about = {}
-if not VERSION:
-    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-    with open(os.path.join(here, project_slug, '__version__.py')) as f:
-        exec(f.read(), about)
-else:
-    about['__version__'] = VERSION
-
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
-
-        self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
-
-        sys.exit()
-
-
-# Where the magic happens:
-setup(
-    name=NAME,
-    version=about['__version__'],
-    description=DESCRIPTION,
+setuptools.setup(
+    name="qdata",
+    version=VERSION,
+    description="Python SDK for getting data quickly",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-    # If your package is a single module, use this instead of 'packages':
-    # py_modules=['mypackage'],
-
+    long_description_content_type="text/markdown",
+    url="https://github.com/longxiaofei/spider-BaiduIndex",
+    author="longxiaofei",
+    author_email="libra.19951002@gmail.com",
+    packages=setuptools.find_packages(
+        exclude=["tests", "*.tests", "*.tests.*", "tests.*", "test.py"]
+    ),
+    install_requires=[
+        "requests>=2.19.1"
+    ],
     # entry_points={
-    #     'console_scripts': ['mycli=mymodule:cli'],
+    #     'console_scripts': [
+    #         'tobe=tobe:main'
+    #     ],
     # },
-    install_requires=REQUIRED,
-    extras_require=EXTRAS,
-    include_package_data=True,
-    license='MIT',
     classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        "Programming Language :: Python",
     ],
-    # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
+    keywords='data sdk',
+    include_package_data=True
 )
